59420e25e38e7421c0631b4b90f330290032de62
BitRanger <63799660+b1tranger@users.noreply.github.com>
2026-08-23 09:31:58 +0600

    final lab code

--- Lab_Codes/Header_File/new.h
+++ Lab_Codes/Header_File/new.h
@@ -7,3 +7,27 @@ int sum(int a, int b)
     return a+b;
 
 }
+
+int sub(int a, int b)
+{
+    return a-b;
+
+}
+
+int mul(int a, int b)
+{
+    return a*b;
+
+}
+
+float divi(float a, float b)
+{
+    if(b==0){
+        cout << "Division of 0 --> Invalid" << endl;
+    }
+    else{
+    return a/b;
+    }
+
+
+}
